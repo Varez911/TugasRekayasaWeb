989ee101cc96fe5e1de9b8e94f97b7f5545af2cf
Varez911 <varez.yeyez@gmail.com>
2020-10-19 06:06:27 +0700

    Membuat cache untuk menyimpan tema

--- TugasWeb2/script.js
+++ TugasWeb2/script.js
@@ -3,23 +3,41 @@ const tradisionalButton = document.getElementById('tradisional');
 
 const body = document.body;
 
+// Saving Cache theme to local storage
+const theme = localStorage.getItem('theme');
+
+if (theme){
+  body.classList.add(theme);
+  if (theme == 'industrial'){
+    document.getElementById('icon-tradisional').style.opacity=0;
+    document.getElementById('icon-industrial').style.opacity=1;
+  }
+  else if(theme == 'tradisional'){
+    document.getElementById('icon-tradisional').style.opacity=1;
+    document.getElementById('icon-industrial').style.opacity=0;
+  }
+  
+}
+
 // Button Click Event
 
 industrialButton.onclick = () => {
   
   document.getElementById('icon-tradisional').style.opacity=0;
   document.getElementById('icon-industrial').style.opacity=1;
-  //document.getElementById('icon').src='img/industrial.png';
-  //document.getElementById('icon').style.opacity=1;
+  
   body.classList.replace('tradisional', 'industrial');
+  localStorage.setItem('theme', 'industrial');
+
   document.getElementById('title').innerHTML='Industrial Matrix';
 };
 
 tradisionalButton.onclick = () => {
-  //document.getElementById('icon').src='img/tradisional.png';
   document.getElementById('icon-tradisional').style.opacity=1;
   document.getElementById('icon-industrial').style.opacity=0;
-  //document.getElementById('bg-image').style.background= 'url(img/Flower.gif)';
+
   body.classList.replace('industrial', 'tradisional');
+  localStorage.setItem('theme', 'tradisional');
+
   document.getElementById('title').innerHTML='Tradisional Matrix';
 };
